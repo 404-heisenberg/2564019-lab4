7287bfe04e1a769d8196c07efcc9c25ad07e95ba
Renda Mudau <107690881+404-heisenberg@users.noreply.github.com>
2026-03-02 16:10:03 +0200

    searching for country borders works

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ async function searchCountry(countryName) {
   try {
     loadingSpinner.classList.remove("hidden");
 
-    const url = `https://restcountries.com/v3.1/name/${countryName}`;
+    const url = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
     const response = await fetch(url);
     const data = await response.json();
 
@@ -45,6 +45,8 @@ async function searchCountry(countryName) {
     `;
 
     // get bordering countries
+    const borderingCountries = country.borders;
+    searchBorders(borderingCountries);
   } catch (error) {
     const errorMsg = (document.getElementById("error-message").textContent =
       "Country not found.");
@@ -53,5 +55,28 @@ async function searchCountry(countryName) {
     c;
   } finally {
     loadingSpinner.classList.add("hidden");
+  }
+}
+
+const borderingCountriesSec = document.getElementById("bordering-countries");
+
+async function searchBorders(borderingCountries) {
+  // for each bordering code, fetch that countries info
+  for (const code of borderingCountries) {
+    const response = await fetch(
+      `https://restcountries.com/v3.1/alpha/${code}`,
+    );
+    const data = await response.json();
+    const country = data[0];
+    let bCountry = document.createElement("section");
+    bCountry.classList.add("country-card");
+    bCountry.innerHTML = `
+      <h2>${country.name.common}</h2>
+      <p><strong>Capital:</strong> ${country.capital[0]}</p>
+      <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
+      <p><strong>Region:</strong> ${country.region}</p>
+      <img src="${country.flags.svg}" alt="${country.name.common} flag">
+      `;
+    borderingCountriesSec.appendChild(bCountry);
   }
 }
