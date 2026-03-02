9a416528fc4e504833dfa53c3e79c4fc23488186
Renda Mudau <107690881+404-heisenberg@users.noreply.github.com>
2026-03-02 15:46:54 +0200

    country searching working with errors

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let searchBtn = document.getElementById("search-btn");
 let countryInput = document.getElementById("country-input");
+let loadingSpinner = document.getElementById("loading-spinner");
+
+loadingSpinner.classList.add("hidden");
 
 searchBtn.addEventListener("click", () => {
-  let countryName = countryInput.value.trim();
+  const countryName = countryInput.value.trim();
 
   if (!countryName) return;
 
@@ -13,7 +16,7 @@ searchBtn.addEventListener("click", () => {
 
 countryInput.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
-    let countryName = countryInput.value.trim();
+    const countryName = countryInput.value.trim();
 
     if (!countryName) return;
 
@@ -24,7 +27,31 @@ countryInput.addEventListener("keydown", (e) => {
 });
 
 async function searchCountry(countryName) {
-  const url = `https://restcountries.com/v3.1/name/${countryName}`;
-  const response = await fetch(url);
-  const data = await response.json();
+  try {
+    loadingSpinner.classList.remove("hidden");
+
+    const url = `https://restcountries.com/v3.1/name/${countryName}`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    // get the country then process its data
+    const country = data[0];
+    document.getElementById("country-info").innerHTML = `
+        <h2>${country.name.common}</h2>
+        <p><strong>Capital:</strong> ${country.capital[0]}</p>
+        <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
+        <p><strong>Region:</strong> ${country.region}</p>
+        <img src="${country.flags.svg}" alt="${country.name.common} flag">
+    `;
+
+    // get bordering countries
+  } catch (error) {
+    const errorMsg = (document.getElementById("error-message").textContent =
+      "Country not found.");
+    document.getElementById("country-info").innerHTML = " ";
+
+    c;
+  } finally {
+    loadingSpinner.classList.add("hidden");
+  }
 }
